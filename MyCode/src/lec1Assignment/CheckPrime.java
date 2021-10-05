dacf2e0f0e71ea6a91ba1178fe45850d93a7026d
KulyashDahiya <kulyashdahiya77@gmail.com>
2021-10-05 11:04:40 +0530

    Lec1Assignment

--- MyCode/src/lec1Assignment/CheckPrime.java
+++ MyCode/src/lec1Assignment/CheckPrime.java
@@ -1,4 +1,5 @@
 package lec1Assignment;
+
 import java.util.*;
 
 public class CheckPrime {
@@ -7,16 +8,24 @@ public class CheckPrime {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int n=2;
-		
-		while(n<N) {
-			for(int i=1; i<n; i++) {
-				if(n%i==0) {
-					
+		int n = 2;
+
+		while (n <= N) {
+			boolean flag = true;
+			int i=2;
+			int x = (int)Math.sqrt(n);
+			while(i <= x){
+				if (n % i == 0) {
+					flag = false;
+					break;
 				}
+				i++;
 			}
+			if (flag) {
+				System.out.println(n);
+			}
+			n++;
 		}
-
+		sc.close();
 	}
-
 }
