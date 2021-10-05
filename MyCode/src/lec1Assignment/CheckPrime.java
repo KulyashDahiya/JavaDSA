549891d7a0de7270114f2f0577ee5abeee269959
KulyashDahiya <kulyashdahiya77@gmail.com>
2021-10-05 08:08:24 +0530

    update

---
+++ MyCode/src/lec1Assignment/CheckPrime.java
@@ -0,0 +1,22 @@
+package lec1Assignment;
+import java.util.*;
+
+public class CheckPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int n=2;
+		
+		while(n<N) {
+			for(int i=1; i<n; i++) {
+				if(n%i==0) {
+					
+				}
+			}
+		}
+
+	}
+
+}
