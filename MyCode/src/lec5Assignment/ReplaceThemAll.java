bb2c0ea71925896cb672ffa654818ba42d4180c0
KulyashDahiya <kulyashdahiya77@gmail.com>
2021-10-14 19:42:15 +0530

    Assignment

--- MyCode/src/lec5Assignment/ReplaceThemAll.java
+++ MyCode/src/lec5Assignment/ReplaceThemAll.java
@@ -1,4 +1,24 @@
 package lec5Assignment;
 
+import java.util.Scanner;
+
 public class ReplaceThemAll {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        if(n != 0) {
+            long sum = 0, t = 1;
+            while (n != 0) {
+                int r = (int) (n % 10);
+                if (r != 0)
+                    sum += r * t;
+                else
+                    sum += 5 * t;
+                t *= 10;
+                n /= 10;
+            }
+            System.out.println(sum);
+        }
+
+    }
 }
