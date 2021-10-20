41cff4dfde3305c7cbeaf82de17d85b412c32888
KulyashDahiya <kuyashdahiya77@gmail.com>
2021-10-20 18:46:44 +0530

    commit

--- MyCode/src/lec8/SelectionSort.java
+++ MyCode/src/lec8/SelectionSort.java
@@ -1,7 +1,29 @@
 package lec8;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SelectionSort {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i <= arr.length; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void swap(int[] arr, int a)
+    {
+        int temp = arr[a];
+        arr[a] = arr[a+1];
+        arr[a+1] = temp;
     }
 }
