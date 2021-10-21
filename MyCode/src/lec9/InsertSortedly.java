4a9c82ad65bce0035215066996147109ab0f7545
KulyashDahiya <kulyashdahiya77@gmail.com>
2021-10-21 19:36:53 +0530

    InsertedSortedly

---
+++ MyCode/src/lec9/InsertSortedly.java
@@ -0,0 +1,15 @@
+package lec9;
+
+import java.util.Scanner;
+
+public class InsertSortedly {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+    }
+}
