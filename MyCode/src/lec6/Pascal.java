85e31f3a169a80cd72a7fc4288d204b0d7f8ec26
KulyashDahiya <kulyashdahiya77@gmail.com>
2021-10-22 08:17:13 -0700

    PascalTriangle

--- MyCode/src/lec6/Pascal.java
+++ MyCode/src/lec6/Pascal.java
@@ -4,7 +4,22 @@ public class Pascal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
-
+        int nsp = n;
+        for(int i = 1; i <=n; i++)
+        {
+            int csp = 1;
+            while(csp <= nsp){
+                System.out.print(" ");
+                csp++;
+            }
+            int ncr = 1;
+            System.out.print(ncr + " ");
+            for (int r = 2; r <=i ; r++) {
+                ncr = (ncr * ((i-1)-(r-1)+1))/(r-1);
+                System.out.print(ncr + " ");
+            }
+            System.out.println();
+            nsp--;
+        }
     }
 }
